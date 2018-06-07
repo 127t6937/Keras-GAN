9d0afdb9822c76247463de74be456f4621a97cb6
S.Yasaka <127t6937@gmail.com>
2018-06-07 11:48:02 +0900

    ganからBatchNorm削除

--- gan/gan.py
+++ gan/gan.py
@@ -29,7 +29,7 @@ class GAN():
         self.discriminator.compile(loss='binary_crossentropy',
             optimizer=optimizer,
             metrics=['accuracy'])
-
+n
         # Build the generator
         self.generator = self.build_generator()
 
@@ -55,13 +55,12 @@ class GAN():
 
         model.add(Dense(256, input_dim=self.latent_dim))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
         model.add(Dense(512))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
+
         model.add(Dense(1024))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
+
         model.add(Dense(np.prod(self.img_shape), activation='tanh'))
         model.add(Reshape(self.img_shape))
 
